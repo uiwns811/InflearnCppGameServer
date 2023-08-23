2d4af2b1b706670b654d65bbcb85ca0b4046a57f
uiwns811 <dltnals5809@gmail.com>
2023-08-23 20:05:08 +0900

    SpinLock - 서로 스핀락 무한루프 돌면 CPU 점유율이 높아진다. - 만약 대기 시간이 길다면 CPU 낭비 갑

--- Server/CppGameServer/GameServer.cpp
+++ Server/CppGameServer/GameServer.cpp
@@ -3,30 +3,80 @@
 #include "CorePch.h"
 #include <thread>
 #include <atomic>
-#include <mutex>
-#include "AccountManager.h"
-#include "UserManager.h"
+#include <mutex> 
+using namespace std;
 
-void Func1()
+class SpinLock
 {
-	for (int32 i = 0; i < 1; i++) {
-		UserManager::Instance()->ProcessSave();
+public:
+	void lock()
+	{
+		// CAS (Compare-And-Swap)
+		bool expected = false;
+		bool desired = true;
+
+		// CAS 의사코드
+		/*if (_locked == expected) {
+			_locked = desired;
+			return true;
+		}
+		else {
+			expected = _locked;
+			return false;
+		}*/
+
+		while (_locked.compare_exchange_strong(expected, desired) == false)
+		{
+			expected = false;
+		}
+
+		//while (_locked)
+		//{
+		//	
+		//}
+		//_locked = true;
+	}
+
+	void unlock()
+	{
+		_locked.store(false);
+	}
+
+private:
+	atomic<bool> _locked = false;
+};
+
+int32 sum = 0;
+mutex m;
+SpinLock spinlock;
+
+void Add()
+{
+	for (int32 i = 0; i < 100'000; i++)
+	{
+		lock_guard<SpinLock> guard(spinlock);
+		sum++;
 	}
 }
 
-void Func2()
+void Sub()
 {
-	for (int32 i = 0; i < 1; i++) {
-		AccountManager::Instance()->ProcessLogin();
+	for (int32 i = 0; i < 100'000; i++)
+	{
+		lock_guard<SpinLock> guard(spinlock);
+		sum--;
 	}
 }
 
 int main()
 {
-	thread t1(Func1);
-	thread t2(Func2);
+	thread t1(Add);
+	thread t2(Sub);
 	t1.join();
 	t2.join();
 
-	cout << "Jobs Done !" << endl;
+	cout << sum << endl;
 }
+
+// volatile : 컴파일러에게 최적화하지 말아달라고 요청
+// - C# : 메모리베리어, 가시성 기능 추가
