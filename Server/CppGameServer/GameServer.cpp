372b9970395a6bb1e4dc3544e748b1524dcf313c
uiwns811 <dltnals5809@gmail.com>
2023-08-23 18:41:37 +0900

    Lock : mutex, lock_guard, unique_lock

--- Server/CppGameServer/GameServer.cpp
+++ Server/CppGameServer/GameServer.cpp
@@ -3,48 +3,75 @@
 #include "CorePch.h"
 #include <thread>
 #include <atomic>
+#include <mutex>
 
-atomic<int32> sum = 0;
+vector<int32> v;
+mutex m;
 
-void Add()
+template<typename T>
+class LockGuard
 {
-	for (int32 i = 0; i < 1'000'000; i++)
-		//sum++;
-		sum.fetch_add(1);
+public:
+	LockGuard(T& m) {
+		_mutex = &m;
+		_mutex->lock();
+	}
 
-	// 디스어셈블리에서 확인한 sum++의 매커니즘
-	// int32 eax = sum;
-	// eax = eax + 1;
-	// sum = eax;
-}
+	~LockGuard()
+	{
+		_mutex->unlock();
+	}
+private:
+	T* _mutex;
+};
 
-void Sub()
+void Push()
 {
-	for (int32 i = 0; i < 1'000'000; i++)
-		//sum--;
-		sum.fetch_add(-1);
+	for (int32 i = 0; i < 10000; i++)
+	{
+		// 자물쇠 잠그기
+		//m.lock();
+		LockGuard<mutex> lockGuard(m);
+
+		v.push_back(i);
+
+		if (i == 5000) {
+			//m.unlock();
+			break;
+		}
+		
+		// 자물쇠 풀기
+		//m.unlock();
+	}
 }
 
 int main()
 {
-	Add();
-	Sub();
-	cout << sum << endl;
+	// v.reserve(20000);
+	thread t1(Push);
+	thread t2(Push);
 
-	thread t1(Add);
-	thread t2(Sub);
 	t1.join();
 	t2.join();
-	cout << sum << endl;
+
+	cout << v.size() << endl;
 }
 
-// 멀티스레드 환경에서 데이터를 동시에 접근하게 될 때 문제가 생길 수 있다
-// 위에 기술한 sum++의 3개의 과정은 명령어의 실행이 Atomic하다고 보장할 수 없음
+// vector : push_back 할 때 잔여 메모리가 없으면 새로 할당받아서 기존 원소들의 내용을 복사하고 기존 메모리를 해제함
+// 이러한 상황에서 두 스레드가 v에 동시에 접근하게 되면, t1에 의해 해제된 기존 v의 메모리에 t2가 접근하게 되는 문제가 발생할 수 있음.
+// 또한, 두 스레드가 동시에 삽입한다면 같은 위치에 데이터가 두 번 기입되어 일부 데이터가 분실될 수 있음 (v.size() != 20000)
+// - 이러한 자료구조는 멀티스레드에 안전하지 않음.
 
-// Atomic : All or Nothing (실행이 되거나 아예 안되거나)
+// Lock
+// : 먼저 들어갈 애가 Lock하면, 다른 애는 접근한 애가 Unlock할 때까지 접근할 수 없음
+// Mutual Exclusive (상호배타적)
+// but 개오래걸림
 
-// Atomic Class로 감싸주면 3단계의 명령어 실행이 한 번에 수행된다.
-// - Atomic한 실행이 보장된다 !
+// Lock 주의할 점
+// 1. Lock을 재귀적으로 걸 수 있냐? (웬만하면 허용하는게 편함)
+// - Mutex (X), Recursive Mutex (o)
+// 2. Unlock하지 않는 경우
+// - 프로그램이 영 영 끝나지 않음 ..
+// - RAII 패턴 (Resource Acquisition Is Initialization) 활용
+// - : 클래스를 이용해서 자동으로 lock, unlock 관리
 
-// 둘 중 먼저 접근한 애가 먼저 수행하고,
-// 그 아이의 수행이 완료될 때까지 CPU가 접근할 수 없도록 막아서 다른 애는 대기
