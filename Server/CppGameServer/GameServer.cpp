99cb3bb7bd6b160c92355011a8b6a222f793d968
uiwns811 <dltnals5809@gmail.com>
2023-09-13 19:42:24 +0900

    future : async, promise, packaged_task

--- Server/CppGameServer/GameServer.cpp
+++ Server/CppGameServer/GameServer.cpp
@@ -3,78 +3,112 @@
 #include "CorePch.h"
 #include <thread>
 #include <atomic>
-#include <mutex> 
-#include <Windows.h>
+#include <mutex>
+#include <future>
 using namespace std;
 
-mutex m;
-queue<int32> q;
-HANDLE handle;
+int64 result;
 
-// [참고] CV : User-Level Object (커널 오브젝트 X)
-condition_variable cv;
-// mutex와 짝지어서 동작
-// event와 유사
- 
-
-//#include <condition_variable>
-//condition_variable_any cv;
-
-void Producer()
+int64 Calculate()
 {
-	while (true) 
-	{
-		// 1. Lock을 잡고
-		// 2. 공유 변수 값 수정
-		// 3. Lock을 풀고
-		// 4. 조건변수를 통해 다른 쓰레드에게 통지
+	int64 sum = 0;
 
-		{
-			unique_lock<mutex> lock(m);
-			q.push(100);
-		}
+	for (int32 i = 0; i < 100'000; i++)
+		sum += i;
 
-		cv.notify_one();		// wait중인 쓰레드가 있으면 딱 1개를 깨운다.
-		
-		//this_thread::sleep_for(100ms);
-	}
+	return sum;
 }
 
-void Consumer()
+void PromiseWorker(std::promise<string>&& promise)
 {
-	while (true) {
-		::WaitForSingleObject(handle, INFINITE);
+	promise.set_value("Secret Message");
+}
 
-		// 이 상황에서 WaitForSingleObject와 lock 잡는 부분 사이에서 다른 애가 lock을 잡을 수 있음
-
-		unique_lock<mutex> lock(m);
-		if (!q.empty()) {
-			int32 data = q.front();
-			q.pop();
-			cout << q.size() << endl;
-		}
-	}
+void TaskWorker(std::packaged_task<int64(void)>&& task)
+{
+	task();
 }
 
 int main()
 {
-	// 커널 오브젝트 : 커널에서 관리하는 객체
-	// Usage Count : 이 오브젝트를 몇 명이 사용하고 있는지
-	// Signal (파란불) / Non-Signal (빨간불) >> boolean
-	// Auto / Manual  >> boolean
+	// 동기 (Synchronous) 실행
+	//int64 sum = Calculate();
+	//cout << sum << endl;
 
-	handle = ::CreateEvent(NULL/*보안속성*/, FALSE/*Manual Reset : True - 수동, False - 자동*/, FALSE /*bInitialState*/, NULL);
+	// std::future : 단기 알바
+	{
+		// 옵션
+		// 1. deferred : 지연해서 실행하세요
+		// 2. async : 별도의 쓰레드를 만들어서 실행하세요
+		// 3. deferred | async : 둘 중 알아서 골라주세요
 
-	thread t1(Producer);
-	thread t2(Consumer);
-	
-	t1.join();
-	t2.join();
+		std::future<int64> future = std::async(std::launch::async, Calculate);
+		// async : Calculate의 완료 여부에 상관없이 리턴함
 
-	::CloseHandle(handle);
+		int sum = future.get();		// 결과물이 이제서야 필요하다!
+
+		//std::future_status status = future.wait_for(1ms);		// n초동안 기다려주세요
+		//if (status == future_status::ready) {
+		//	// 일감 완료 : 여기서 처리
+		//}
+
+		//future.wait();				// wait_for 무한정
+
+		// 동기 : 호출하는 순간 실행
+		// 비동기 : 실행 시점이 뒤로 밀린다.
+
+		// async : 사실상 멀티쓰레드 환경. (별도의 쓰레드가 Calculate를 병렬로 실행해줌)
+		// deferred : 멀티쓰레드는 아님
+
+		class Knight {
+		public:
+			int64 GetHp() { return 100; }
+		};
+
+		Knight knight;
+		std::future<int64> future2 = std::async(std::launch::async, &Knight::GetHp, knight);
+	}
+
+	// std::promise (future 객체를 만들어주는 두 번째 방법)
+	{
+		// 미래(std::future)에 결과물을 반환해줄거라 약속 (std::promise)
+		// promise는 다른 쓰레드에게 넘기고, future는 우리가 갖고 있을 거임
+		std::promise<string> promise;
+		std::future<string> future = promise.get_future();
+
+		thread t(PromiseWorker, std::move(promise));
+		string message = future.get();
+		cout << message << endl;
+		t.join();	
+	}
+
+	// std::packaged_task
+	{
+		// task : Calculate를 다른 쓰레드에서 호출해주세요~ 라고 일감 단위로 만들어 줌
+		std::packaged_task<int64(void)> task(Calculate);
+		std::future<int64> future = task.get_future();
+
+		// Calculate의 결과를 future로 받아올 수 있다
+
+		std::thread t(TaskWorker, std::move(task));
+		int64 sum = future.get();
+		cout << sum << endl;
+		t.join();
+	}
 }
 
-// 커널 오브젝트를 통해 프로세스 간의 동기화를 구현할 수 있다.
+// 잠깐 기능을 사용하기 위해 쓰레드 관리를 하기보다는 future를 사용하자.
 
-// SpinLock : 유저레벨에서 일어나는 동기화
-// Event : 커널단계까지 개입하는 동기화 (활용성이 높지만 비용이 높다)
+// future : 언젠가 미래에 결과물을 뱉어줄거야! (당장은 없을 수도 있지만 get하는 순간에는 결과물을 얻어올거야)
+
+
+// 결론
+// mutex, condition_variable까지 가지 않고 단순한 애들을 처리할 수 있는 것들
+// - 일회성 이벤트에 유용 ! (무한루프 ㄴㄴ 한 번만 해야 하는 것)
+
+// async : Calculate 처리를 위한 전용 쓰레드를 새로 만들어서 실행
+// Task : 이미 존재하는 쓰레드에게 일감(Task)을 던져주고 실행하도록 함. 결과물은 future로 받음
+
+// 1. async : 원하는 함수를 비동기적으로 실행
+// 2. promise : 결과물을 promise를 통해 future로 받아줌
+// 3. packaged_task : 원하는 함수의 실행 결과를 packaged_task를 통해 future로 받아줌
