f3f7f412904208bfe71cdc934a52ef366e03aa83
uiwns811 <dltnals5809@gmail.com>
2023-09-14 19:52:17 +0900

    Memory order : atomic연산과 CAS (compare_exchange_strong, compare_exchange_weak)

--- Server/CppGameServer/GameServer.cpp
+++ Server/CppGameServer/GameServer.cpp
@@ -6,80 +6,72 @@
 #include <mutex>
 #include <Windows.h>
 
-int32 x = 0;
-int32 y = 0;
-int32 r1 = 0;
-int32 r2 = 0;
-
-volatile bool ready;
-
-void Thread_1()
-{
-	while (!ready)
-		;
-	y = 1;		// Store y
-	r1 = x;		// Load x
-}
-
-void Thread_2()
-{
-	while (!ready)
-		;
-	x = 1;		// store x
-	r2 = y;		// load y
-}
+atomic<bool> flag = false;
 
 int main()
 {
-	int32 count = 0;
-	
-	while (true) {
-		ready = false;
-		count++;
 
-		x = y = r1 = r2 = 0;
+	//flag = true;
+	flag.store(true, memory_order::memory_order_seq_cst);
 
-		thread t1(Thread_1);
-		thread t2(Thread_2);
+	//bool val = flag;
+	bool val = flag.load(memory_order::memory_order_seq_cst);
 
-		ready = true;
-		
-		t1.join();
-		t2.join();
+	cout << flag.is_lock_free() << endl;
+	// 1 == true == lock이 없다.
+	// -> 이미 CPU 내부적으로 Lock을 걸 필요가 없는 atomic한 연산이다.
 
-		if (r1 == 0 && r2 == 0)
-			break;
+	// 이전 flag값을 prev에 넣고 flag 값을 수정
+	{
+		// bool prev = flag;
+		// flag = true;
+		bool prev = flag.exchange(true);
+		// -> flag의 값을 true로 변경하고, 변경 이전의 값을 리턴.
+		// 위의 두 연산을 원자적으로 실행하는 함수
 	}
 
-	cout << count << "번 만에 빠져나옴" << endl;
+	// CAS (Compare-And-Swap) 조건부 수정
+	{
+		bool expected = false;
+		bool desired = true;
+		flag.compare_exchange_strong(expected, desired);
+
+		// Spurious Failure (가짜실패)
+		if (flag == expected) {
+			// 다른 쓰레드의 방해를 받아  interruption을 받아 중간에 실패할 수 있음
+			// if (묘한 상황) return false;
+
+			// expected = flag;
+			flag = desired;
+			return true;
+		}
+		else {
+			expected = flag;
+			return false;
+		}
+		// compare_exchange_strong : 위 연산을 원자적으로 실행해준다.
+		// expected와 같으면 desired로 수정해주고 true를 리턴
+		// expected와 같지 않으면 expected에 flag값을 넣어주고 false 리턴
+	}
+
+	bool expected = false;
+	bool desired = true;
+	flag.compare_exchange_weak(expected, desired);
+	// weak : 동작 자체는 동일하지만, 위에 적어놓은 Spurious Failure로 인해 중간에 실패하는 상황이 생길 수 있음
+	// strong : 한바퀴를 더 돌아 성공할 때까지 도전
+
+	// weak를 사용할거면 while루프를 해주는게 일반적.
+	// 성능 차이가 일반적으로 크게 없다.
 }
 
-// 83973번 만에 빠져나옴
-// 거의 동시에 실행되는데 둘 중 먼저 실행되는 애가 있음. 그러면 어쨌든 r1이나 r2나 변경이 될 거임
-// but 멀티쓰레드 환경에서는 이러한 오류 (r1도 r2도 0)가 생길 수 있음
+// 캐시와 CPU 내부 최적화 작업으로 인해 멀티쓰레드 환경에서는 문제가 발생할 수 있다.
 
-// 왜?
-// 1. 가시성
-// 캐시 : CPU가 쓰거나 읽을 값이 캐시에 있으면 RAM까지 가지 않고 캐시에서 값을 가져옴
-// - CPU의 코어마다 각각의 캐시를 가지고 있음.
-// 읽어온 값, 쓰는 값이 실제 메모리에 쓰지 않고, 캐시에만 썼을 수도 있음
-// 단일쓰레드라면 내가 사용하던 캐시의 값에 적용되어 있으니 문제 없지만,
-// 멀티쓰레드에서는 내 캐시에 적용되지 못했고, 램에도 없음
-// == 가시성이 보장되지 않는다.
+// 메모리모델 (Memory Model)
 // 
-// 2. 코드 재배치
-// 컴파일러가 내가 만든 코드를 기계어로 변환 -> CPU가 기계어 실행
-// 컴파일러는 100% 그대로 번역하지 않을 수도 있다.
-// 검수 중 효율성을 위해 코드 순서를 변경할 수 있다.
-// 컴파일러는 멀티쓰레드 환경을 고려하지 않기 때문이다.
-
-// -------------------------------------------
-// CPU는 명령어를 실행할 때 CPU 파이프라인(4가지 단계)을 거친다.
-// 1. Fetch : 명령어 가져오기
-// 2. Decode : 해독
-// 3. Execute : 실행
-// 4. Write-back : 결과를 다시 가져다줌
+// # Atomic (원자적) 연산 : CPU가 한 번에 처리할 수 있는 연산
+// 여러 단계 쪼개서 해야 하면 원자적이지 않음
+//
+// # Atomic 연산에 한해, 모든 쓰레드가 동일 객체에 대해 동일한 수정 순서를 관찰
+// - **동일 객체** 에 대해 **동일 순서**가 중요!!
+// == 과거로는 갈 수 없다. (절대 시간 순서를 반드시 지켜야 한다)
 // 
-// 명령어를 여러 개 요청했을 때,
-// 경우에 따라 순서를 바꿔서 수행하는게 더 효율적이라면 CPU가 알아서 순서를 변경해줄 수 있음
-// 단일 쓰레드 기준으로는 전혀 문제가 없으나, 멀티쓰레드에서는 로직이 꼬일 수 있음.
